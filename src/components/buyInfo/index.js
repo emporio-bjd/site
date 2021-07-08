44e5c04850da7ec4610e697253600f2362876939
Higor Gonçalves Brandão <higorb2000@gmail.com>
2021-07-08 18:52:27 -0300

    fix erro modal histórico

--- src/components/buyInfo/index.js
+++ src/components/buyInfo/index.js
@@ -1,60 +1,42 @@
 import React from 'react'
-import { useEffect, useState } from 'react'
 import './style.css'
 
-import firebase from 'firebase/app'
 import 'firebase/auth'
 import 'firebase/database'
-import firebaseConfig from '../../FIREBASECONFIG.js'
 
 
 function BuyInfo(props) {
 
     const { displayProperty, modalData } = props;
-    const [dataAdmin, setDataAdmin] = useState([])
 
-    // useEffect(() => {
-
-    //     if (!firebase.apps.length)
-    //         firebase.initializeApp(firebaseConfig);
-
-    //     var firebaseRef = firebase.database().ref('providers-requests/');
-
-    //     firebaseRef.on('value', (snapshot) => {
-
-    //         if (snapshot.exists()) {
-
-    //             var data = snapshot.val()
-    //             var temp = Object.keys(data).map((key) => data[key])
-    //             setDataAdmin(temp)
-
-    //         }
-
-    //     })
-
-    // }, [])
 
     return (
 
         <div style={{ display: displayProperty }} className='modalBuyInfo'>
 
-            {console.log("props:",props)}
+            {console.log("props:", props)}
 
             <main id='mainBuyInfo' >
 
                 <div className="buyInfoTitle">
                     <h2>Sua lista de produtos adquiridos: {modalData.provider}</h2>
 
-                    {modalData.listItem.map((item)=>{
-
-                        return <p>{item.product}</p>
-
-                    })}
-                    
                     <h4>{modalData.listTitle}</h4>
                     <h4>{modalData.orderDate} - {modalData.orderTime}</h4>
 
-                    {console.log(modalData.listItem)}
+
+
+                    {modalData.listItem != undefined ?
+                        modalData.listItem.map((item) => {
+
+                            return <p>{item.product}</p>
+
+                        }
+                        ) : <p></p>
+
+                    }
+
+
 
                 </div>
 
@@ -63,7 +45,7 @@ function BuyInfo(props) {
         </div>
 
     )
-
 }
+
 
 export default BuyInfo
