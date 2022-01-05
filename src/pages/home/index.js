f30719cd838ed9fd6a69e7f1165682296f1c0340
emporio-bjd <87431447+emporio-bjd@users.noreply.github.com>
2022-01-05 13:11:46 -0300

    mudança do link de redirecionamento pra loja

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -51,7 +51,7 @@ function Home() {
 
                 <div className="buttonsWrapper">
 
-                    <a target="_blank" rel="noreferrer" href="https://www.emporiobomjardim.com" className="btnRedirect">Ir para loja</a>
+                    <a target="_blank" rel="noreferrer" href="https://form.jotform.com/213092293869667" className="btnRedirect">Ir para loja</a>
                     <Link to="/Quem-somos-nos" className="btnAboutUs" >Saiba mais</Link>
 
                 </div>
